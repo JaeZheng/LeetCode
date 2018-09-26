e3557f3acc8170d0c96be65d1484873dbb78d5e8
JaeZheng <846265697@qq.com>
2018-09-26 12:26:18 +0800

    add 22

---
+++ 22.py
@@ -0,0 +1,16 @@
+class Solution:
+    def generateParenthesis(self, n):
+        """
+        :type n: int
+        :rtype: List[str]
+        """
+        dp = [[] for i in range(n+1)]
+        dp[0].append("")
+        for i in range(n+1):
+            for j in range(i):
+                dp[i] += ["("+x+")" + y for x in dp[j] for y in dp[i-j-1]]
+        return dp[n]
+
+s = Solution()
+a = s.generateParenthesis(4)
+print(a)
