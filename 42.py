d31c89e88cb0a3b082e245de1dd814d8cc21bd47
JaeZheng <846265697@qq.com>
2019-09-11 19:45:53 +0800

    add 42

---
+++ 42.py
@@ -0,0 +1,51 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Author  : JaeZheng
+# @Time    : 2019/9/11 18:59
+# @File    : 42.py
+# @Address : https://leetcode-cn.com/problems/trapping-rain-water/
+
+"""
+给定 n 个非负整数表示每个宽度为 1 的柱子的高度图，计算按此排列的柱子，下雨之后能接多少雨水。
+
+上面是由数组 [0,1,0,2,1,0,1,3,2,1,2,1] 表示的高度图，在这种情况下，可以接 6 个单位的雨水（蓝色部分表示雨水）。
+
+示例:
+输入: [0,1,0,2,1,0,1,3,2,1,2,1]
+输出: 6
+"""
+
+
+# 暴力解法，对数组中每个元素计算当前元素能接的雨水数量，叠加
+class Solution(object):
+    def trap(self, height):
+        """
+        :type height: List[int]
+        :rtype: int
+        """
+        size = len(height)
+        if size <= 1:
+            return 0
+        abs = 0
+        highest_index = 0
+        highest = 0
+        for i in range(size):
+            if height[i] > highest:
+                highest_index = i
+                highest = height[i]
+        for i in range(size):
+            max_side = 0
+            if i <= highest_index:
+                max_side = max(height[:i])
+            else:
+                for j in range(i, size):
+                    if height[j] > max_side:
+                        max_side = height[j]
+            abs += max_side - height[i]
+        return abs
+
+
+solution = Solution()
+height = [0,1,0,2,1,0,1,3,2,1,2,1]
+result = solution.trap(height)
+print(result)
--- test.py
+++ test.py
@@ -1,3 +1,4 @@
-a = [2,3,1,4]
-a.sort()
-print(a)
+## zip()函数单个参数
+list1 = [1, 2, 3, 4]
+dicted = {k: v for (k,v) in zip(list1, range(len(list1)))}
+print(dicted)
